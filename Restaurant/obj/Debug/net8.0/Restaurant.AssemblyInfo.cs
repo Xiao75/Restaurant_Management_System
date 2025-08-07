b4a0e5120165d26cce10142a582f99ab3568726e
Xiao75 <devrajkeisham89@gmail.com>
2025-08-07 16:44:55 +0530

    Add css

--- Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
+++ Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37420bb47b6152a1e5e524d5d260a3a9bc732289")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd33c05b017b994ef0dcb816d65a1fa5583dd5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
